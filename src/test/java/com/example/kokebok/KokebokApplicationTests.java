fb53cc55e401495de58d21bb21a8accff3baab3a
Aasgrav <88495565+aasgrav@users.noreply.github.com>
2021-10-13 13:24:38 +0200

    Initial commit

---
+++ src/test/java/com/example/kokebok/KokebokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kokebok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokebokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
