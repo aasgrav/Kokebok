6bb3d01dd60ed6e7b08824f9b47abc7e3082b9bb
Aasgrav <88495565+aasgrav@users.noreply.github.com>
2021-10-13 14:20:36 +0200

    Legge til Get/Post-mapping for "/forside" i KokebokApplication

--- src/main/java/com/example/kokebok/KokebokApplication.java
+++ src/main/java/com/example/kokebok/KokebokApplication.java
@@ -2,12 +2,29 @@ package com.example.kokebok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
+@Controller
 public class KokebokApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KokebokApplication.class, args);
     }
+
+
+    @GetMapping("/forside")
+    public String forsideGet() {
+        return "forside";
+    }
+
+    @PostMapping("/forside")
+    public String forsidePost() {
+        return "forside";
+    }
+
+
 
 }
