91cef024d42ff8d73737f5f81e130fffbfb359b3
Aasgrav <88495565+aasgrav@users.noreply.github.com>
2021-10-14 14:02:58 +0200

    Legge til to tester.

--- src/test/java/com/example/kokebok/BrukerRegisterTest.java
+++ src/test/java/com/example/kokebok/BrukerRegisterTest.java
@@ -2,6 +2,7 @@ package com.example.kokebok;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -10,11 +11,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
-class KokebokApplicationTests {
+
+public class BrukerRegisterTest {
+
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(BrukerRegister.class).build();
+    }
+
 
     @Test
-    void contextLoads() {
+    public void sjekkeViewNavnSkalVæreRegistrerBruker() throws Exception {
+        mockMvc.perform(get("/bruker/registrer"))
+                .andExpect(view().name("registrerBruker"));
     }
 
 }
---
+++ src/test/java/com/example/kokebok/OppskriftTest.java
@@ -0,0 +1,37 @@
+package com.example.kokebok;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class OppskriftTest {
+
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setup() {
+        KokebokApplication kb = new KokebokApplication();
+        kb.oppskriftRegister = new OppskriftRegister();
+        mockMvc = MockMvcBuilders.standaloneSetup(kb).build();
+    }
+
+
+    @Test
+    public void sjekkeOppskriftsside() throws Exception {
+        String page = "3";
+        String oppskrift = "Oppskrift 24";
+
+        mockMvc.perform(get("/oppskrift?page=" + page + "&oppskriftsnavn=" + oppskrift))
+                .andExpect(view().name("oppskriftDetaljside"))
+                .andExpect(model().attribute("currentPage", page));
+    }
+}
