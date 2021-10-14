ef57f9e03227a781721b7668b5ec0eab7706ce4e
Liv Mette Hamre <livmettehamre@gmail.com>
2021-10-14 10:35:07 +0200

    Har oppdatert Bruker og BrukerRegister og fått knappene dem i mellom til å kjøre.

--- src/main/java/com/example/kokebok/BrukerRegister.java
+++ src/main/java/com/example/kokebok/BrukerRegister.java
@@ -1,10 +1,7 @@
 package com.example.kokebok;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,12 +13,12 @@ public class BrukerRegister {
     //Bruke hashmap til liste over alle brukere. String er brukernavn og unik key. Bruker er objektet bruker.
     Map<String, Bruker> brukere;
 
-/*    BrukerService brukerService;
+//    BrukerService brukerService;
 
-    public BrukerRegister(BrukerService brukerService) {
-        this.brukerService = brukerService;
-        Map<String, Bruker> brukerRegister = new HashMap<>();
-    }*/
+    public BrukerRegister(/*BrukerService brukerService*/) {
+//        this.brukerService = brukerService;
+        this.brukere = new HashMap<>();
+    }
 
     private boolean eksistererBrukernavn(String brukernavn) {
         //Sjekk om brukernavn allerede ligger i lista.
@@ -36,16 +33,21 @@ public class BrukerRegister {
     }
 
     @PostMapping("/registrer")
-    public String registrerBruker(@PathVariable(required = false) String brukernavn, @PathVariable(required = false) String passord, @PathVariable(required = false) String gjentaPassord) {
+    public String registrerBruker(@RequestParam String brukernavn, @RequestParam String passord, @RequestParam String gjentaPassord) { //Her fungerte ikke PathVariable
 /* if (brukerService.save(new User()) {
         return "redirect:/forside"}
     else {
         return "registrerBruker";
     }
  */
-        if (eksistererBrukernavn(brukernavn) || (passord != gjentaPassord)) {
+        System.out.println("brukernavn:" + brukernavn + " passord: " + passord + " gjentaPasord: " + gjentaPassord);
+
+        if (eksistererBrukernavn(brukernavn) || !(passord.equals(gjentaPassord))) {
+            System.out.println("Godkjenner ikke brukerinformasjon");
+//            return "redirect:/bruker/registrer";
             return "registrerBruker";
         } else {
+            System.out.println("Brukerinformasjon godkjent");
             brukere.put(brukernavn, new Bruker(brukernavn, passord));
             return "redirect:/forside";
         }
