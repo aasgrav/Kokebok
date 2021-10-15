2289e939bd9ddcc80ed0dbe0b7dfa16492dfb60b
Liv Mette Hamre <livmettehamre@gmail.com>
2021-10-15 08:20:39 +0200

    Endret kode slik at man kan legge til favoritter i en egen liste.

--- src/main/java/com/example/kokebok/KokebokApplication.java
+++ src/main/java/com/example/kokebok/KokebokApplication.java
@@ -54,6 +54,14 @@ public class KokebokApplication {
         return "oppskriftDetaljside";
     }
 
-
+    //Side med markerte favorittoppskrifter
+    @GetMapping("/mineOppskrifter")
+    public String mineOppskrifterGet (HttpSession session, Model model, @RequestParam(required = false, defaultValue = "1") String page) {
+        int pageSize = 10;
+        model.addAttribute("oppskrifterOnPage",  oppskriftRegister.getPage(Integer.parseInt(page), pageSize));
+        model.addAttribute("currentPage", Integer.parseInt(page));
+        model.addAttribute("totalNumberOfPages", oppskriftRegister.numberOfPages(pageSize));
+        return "oppskrifter";
+    }
 
 }
--- src/main/java/com/example/kokebok/brukerService.java
+++ src/main/java/com/example/kokebok/brukerService.java
@@ -19,8 +19,6 @@ public class brukerService {
         if(!passord.matches(".*\\d.*")){
             reglerOverholdt = false;
         }
-
         return reglerOverholdt;
     }
-
 }
