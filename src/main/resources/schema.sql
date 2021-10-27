939e40550ae0bbf696eb9e0ee82edf255fb83d85
Aasgrav <88495565+aasgrav@users.noreply.github.com>
2021-10-27 14:36:00 +0200

    Oppdatere sql for oppskrifter-tabell.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,12 @@ brukernavn varchar(20) NOT NULL,
 passord varchar(20) NOT NULL
 );
 
+CREATE TABLE oppskrifter(
+    ID int auto_increment,
+    Oppskriftstittel varchar(64) NOT NULL,
+    Ingredienser varchar(256),
+    Allergier varchar(256),
+    Kategori varchar(32),
+    Oppskriftstekst varchar(2000),
+    PRIMARY KEY(ID)
+);
