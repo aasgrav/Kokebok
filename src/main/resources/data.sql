e4d3d5d8689c2a6c44aa9e4ad41ead6cf12c281d
Liv Mette Hamre <livmettehamre@gmail.com>
2021-10-28 12:54:27 +0200

    La til tabell og oppdaterte bruker-tabell.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -36,4 +36,14 @@ VALUES ('Kålrabistappe6', 'Kålrot, Gulrøtter, Fløte, Smør, Salt, Pepper', '
        ('Lynrask linsegryte6', 'Hermetiske røde linser, Buljongterning, Currypaste, Olje, Salt, Pepper, Hvitløk, Løk, Kokosmelk, Hermetiske tomater', '', 3, 'Stek løk til den er blank. Ha i hvitløk og stek i 1 minutt. Ha i currypaste og stek i 2 minutter. Ha i buljong, kokosmelk og hermetiske tomater og kok opp i 5 minutter. Tilsett skylte linser. Smak til med salt og pepper.'),
        ('Bruschetta6', 'Mozzarella, Cherrytomater, Olivenolje, Salt, Pepper, Hvitløk, Basilikum, Brød', 'Gluten', 0, 'Ta skivet brød, pensle med olivenolje og stek de raskt i ovnen. Bland hakkede tomater, olivenolje, finhakket hvitløk, hakket basilikum, salt og pepper i en bolle. Fordel blandingen på brødskivene og topp med noen skiver mozzarella.');
 
+insert into brukere
+values ('lmh', 'Passord123'),
+       ('bruker1', 'Passord12'),
+       ('bruker2', 'Passord12'),
+       ('bruker3', 'Passord12');
 
+
+insert into favoriserte_oppskrifter
+values ('bruker1', 1),
+       ('bruker2', 2),
+       ('bruker3', 3);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,19 +2,30 @@ create database if not exists kokebok;
 
 use kokebok;
 
-CREATE TABLE brukere(
-brukernavn varchar(20) NOT NULL,
-passord varchar(20) NOT NULL
+CREATE TABLE brukere
+(
+    brukernavn varchar(20) NOT NULL,
+    passord    varchar(20) NOT NULL,
+    PRIMARY KEY (brukernavn)
 );
 
-CREATE TABLE oppskrifter(
-    ID int auto_increment,
+CREATE TABLE oppskrifter
+(
+    ID               int auto_increment,
     Oppskriftstittel varchar(64) NOT NULL,
     Ingredienser varchar(256),
     Allergier varchar(256),
-    -- Kategori enum('ITALIENSK', 'SPANSK', 'FRANSK', 'INDISK', 'MIDTØSTEN', 'NORSK', 'NORDISK', 'BRITISK', 'AMERIKANSK', 'KINESISK', 'JAPANSK', 
+    -- Kategori enum('ITALIENSK', 'SPANSK', 'FRANSK', 'INDISK', 'MIDTØSTEN', 'NORSK', 'NORDISK', 'BRITISK', 'AMERIKANSK', 'KINESISK', 'JAPANSK',
     -- 'ANNEN_ASIATISK', 'ØSTEUROPEISK', 'ANNEN_EUROPEISK', 'SØRAMERIKANSK', 'AFRIKANSK', 'AUSTRALSK', 'ANNEN'),
     Kategori int,
     Oppskriftstekst varchar(2000),
     PRIMARY KEY(ID)
+);
+
+CREATE TABLE favoriserte_oppskrifter
+(
+    brukernavn  varchar(20) NOT NULL,
+    oppskriftid int         NOT NULL,
+    FOREIGN KEY (brukernavn) REFERENCES brukere (brukernavn),
+    FOREIGN KEY (oppskriftid) REFERENCES oppskrifter (ID)
 );
